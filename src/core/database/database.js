8e1a9c03737dd853fa97d9540bf38ab333fb6c1a
Retype15 <reynierramos280@gmail.com>
2025-07-13 01:44:51 -0400

    feat: Refactor database connection to support DATABASE_URL and SSL options

--- src/core/database/database.js
+++ src/core/database/database.js
@@ -1,16 +1,30 @@
-// src/core/database/database.js
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false, // Poner en `true` para ver las queries SQL en la consola
-  }
-);
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql',
+    dialectModule: require('mysql2'),
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+      }
+    },
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mariadb',
+      logging: false,
+    }
+  );
+}
 
 module.exports = sequelize;
